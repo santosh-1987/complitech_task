8707a983ea16b953dcee3b85a0fb599fd6979749
Jyoti Singh <jyoti.vskp@gmail.com>
2014-06-08 17:51:25 +0530

    added admin can delete and update and view writer list

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -11,4 +11,35 @@ def create
 end
 end
 
+def index
+	@writers=Writer.all
+end
+
+def destroy
+    @writer = Writer.find(params[:id])
+    if @writer.destroy
+      redirect_to writers_path
+      flash[:notice] = "Destroyed successfully"
+    end
+  end
+
+def edit
+      @writer = Writer.find(params[:id])
+    end
+
+ def update
+    @writer = Writer.find(params[:id])
+      if @writer.update_attributes(params[:writer])
+        flash[:notice] = "Writer Updated Successfully"
+        redirect_to writers_path
+      else
+        render :new
+      end
+end
+
+
+
+
+
+
 end
