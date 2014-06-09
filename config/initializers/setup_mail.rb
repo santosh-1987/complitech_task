2f801e6fe90280df87e219a18c69f841bf4b1fad
Jyoti Singh <jyoti.vskp@gmail.com>
2014-06-09 13:13:28 +0530

    Idented Code !!!

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "imap.gmail.com",
-  :authentication => :login,
-  :user_name => "email2jyotisingh@gmail.com",
-  :password => "jyoti4santosh"
-
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "imap.gmail.com",
+    :authentication => :login,
+    :user_name => "email2jyotisingh@gmail.com",
+    :password => "jyoti4santosh"
 }
-
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
